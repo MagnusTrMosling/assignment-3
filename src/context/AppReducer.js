09d2429fa750768568edee6ef1413c1e7eaf927a
¨MagnusTrM¨ <144000908+MagnusTrM@users.noreply.github.com>
2024-05-27 15:20:56 +0200

    fix appreducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,41 +1,60 @@
-export default (state, action) => {
-	switch(action.type) {
-	  case 'DELETE_TRANSACTION':
-		return {
-		  ...state,
-		  transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-		}
-	  case 'ADD_TRANSACTION':
-		return {
-		  ...state,
-		  transactions: [action.payload, ...state.transactions]
-		}
-	  default:
-		return state;
-	}
-  }
+// export default (state, action) => {
+// 	switch(action.type) {
+// 	  case 'DELETE_TRANSACTION':
+// 		return {
+// 		  ...state,
+// 		  transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+// 		}
+// 	  case 'ADD_TRANSACTION':
+// 		return {
+// 		  ...state,
+// 		  transactions: [action.payload, ...state.transactions]
+// 		}
+// 	  default:
+// 		return state;
+// 	}
+//   }
 
 
-const reducer = (state, action) => {
+// const reducer = (state, action) => {
+// 	switch(action.type) {
+// 	  case 'DELETE_TRANSACTION':
+// 		return {
+// 		  ...state,
+// 		  transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+// 		}
+// 	  case 'ADD_TRANSACTION':
+// 		return {
+// 		  ...state,
+// 		  transactions: [action.payload, ...state.transactions]
+// 		}
+// 	  default:
+// 		return state;
+// 	}
+//   }
+  
+//   // Example usage of the reducer function
+//   const initialState = { transactions: [] };
+//   const action = { type: 'ADD_TRANSACTION', payload: { id: 1, amount: 100 } };
+//   const nextState = reducer(initialState, action);
+  
+//   console.log(nextState);
+
+
+  const appReducer =  (state, action) => {
 	switch(action.type) {
 	  case 'DELETE_TRANSACTION':
-		return {
-		  ...state,
-		  transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-		}
+	  return {
+		...state,
+		transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+	  }
 	  case 'ADD_TRANSACTION':
-		return {
-		  ...state,
-		  transactions: [action.payload, ...state.transactions]
-		}
+	  return {
+		...state,
+		transactions: [action.payload, ...state.transactions]
+	  }
 	  default:
-		return state;
+	  return state;
 	}
-  }
-  
-  // Example usage of the reducer function
-  const initialState = { transactions: [] };
-  const action = { type: 'ADD_TRANSACTION', payload: { id: 1, amount: 100 } };
-  const nextState = reducer(initialState, action);
-  
-  console.log(nextState);
+	}
+	export default appReducer
