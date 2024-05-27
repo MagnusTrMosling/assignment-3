7beef32927cfe2fae3d67fa08e04ebd73e65824a
¨MagnusTrM¨ <144000908+MagnusTrM@users.noreply.github.com>
2024-05-27 15:27:35 +0200

    new version appreducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,47 +1,4 @@
-// export default (state, action) => {
-// 	switch(action.type) {
-// 	  case 'DELETE_TRANSACTION':
-// 		return {
-// 		  ...state,
-// 		  transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-// 		}
-// 	  case 'ADD_TRANSACTION':
-// 		return {
-// 		  ...state,
-// 		  transactions: [action.payload, ...state.transactions]
-// 		}
-// 	  default:
-// 		return state;
-// 	}
-//   }
-
-
-// const reducer = (state, action) => {
-// 	switch(action.type) {
-// 	  case 'DELETE_TRANSACTION':
-// 		return {
-// 		  ...state,
-// 		  transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-// 		}
-// 	  case 'ADD_TRANSACTION':
-// 		return {
-// 		  ...state,
-// 		  transactions: [action.payload, ...state.transactions]
-// 		}
-// 	  default:
-// 		return state;
-// 	}
-//   }
-  
-//   // Example usage of the reducer function
-//   const initialState = { transactions: [] };
-//   const action = { type: 'ADD_TRANSACTION', payload: { id: 1, amount: 100 } };
-//   const nextState = reducer(initialState, action);
-  
-//   console.log(nextState);
-
-
-  const appReducer =  (state, action) => {
+const appReducer =  (state, action) => {
 	switch(action.type) {
 	  case 'DELETE_TRANSACTION':
 	  return {
